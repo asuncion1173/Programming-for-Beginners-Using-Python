c72e75dd687a85521f0cb22c8bad8a08a0c67900
jacobasuncion <asuncion1173@gmail.com>
2022-05-23 21:57:13 +0800

    Commit for Coding Exercise 4.4

---
+++ coding_exer_4_3.py
@@ -0,0 +1,12 @@
+from tkinter import Y
+
+
+x = 332
+y = 2031
+
+if x >= y:
+    print("x is greater than or equal to y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
